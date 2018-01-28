f7e2298ef4cc5f1b7fc9b7d3c52c5f36ef8e4000
Irudayarajasekar <irudhaya.rajashekar@gmail.com>
2018-01-28 15:43:11 +0530

    Commit upto language screen

---
+++ app/src/main/java/com/darkknight/amslerfinal/InstructionsActivity.java
@@ -0,0 +1,94 @@
+package com.darkknight.amslerfinal;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import me.relex.circleindicator.CircleIndicator;
+
+/**
+ * Created by iruda on 28-01-2018.
+ */
+
+public class InstructionsActivity extends AppCompatActivity {
+    LayoutInflater inflater;
+    ViewPager vp;
+    Button gotit;
+    int[] imageArray = {R.drawable.instructionzero,R.drawable.instructionone,R.drawable.instructiontwo,
+                        R.drawable.instructionthree,R.drawable.instructionfour,R.drawable.instructionfive};
+    String[] instructions;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instructionviewpager);
+        inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        vp=(ViewPager)findViewById(R.id.pager);
+        gotit =(Button)findViewById(R.id.gotit);
+        gotit.setVisibility(View.INVISIBLE);
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        instructions=getResources().getStringArray(R.array.instructions);
+        vp.setAdapter(new MyPagesAdapter());
+        vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if(position == 5){
+                    gotit.setVisibility(View.VISIBLE);
+                }else{
+                    gotit.setVisibility(View.INVISIBLE);
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+        indicator.setViewPager(vp);
+    }
+
+    class MyPagesAdapter extends PagerAdapter {
+        @Override
+        public int getCount() {
+            //Return total pages, here one for each data item
+            return imageArray.length;
+        }
+        //Create the given page (indicated by position)
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            System.out.print(position);
+            View page = inflater.inflate(R.layout.instructioncontent, null);
+            ((TextView)page.findViewById(R.id.instructiontext)).setText(instructions[position]);
+            ((ImageView)page.findViewById(R.id.imageview)).setImageResource(imageArray[position]);
+            if(position > 0){
+                ((TextView)page.findViewById(R.id.textview1)).setVisibility(View.INVISIBLE);
+            }
+            ((ViewPager) container).addView(page, 0);
+            return page;
+        }
+        @Override
+        public boolean isViewFromObject(View arg0, Object arg1) {
+            return arg0==(View)arg1;
+        }
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            ((ViewPager) container).removeView((View) object);
+            object=null;
+        }
+    }
+
+}
--- app/src/main/java/com/darkknight/amslerfinal/MainActivity.java
+++ app/src/main/java/com/darkknight/amslerfinal/MainActivity.java
@@ -1,19 +1,64 @@
 package com.darkknight.amslerfinal;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    CardView cardView1,cardView2,cardView3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        cardView1 = (CardView)findViewById(R.id.card_view1);
+        cardView2 = (CardView)findViewById(R.id.card_view2);
+        cardView3 = (CardView)findViewById(R.id.card_view3);
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Locale locale = new Locale("en");
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                Intent i = new Intent(MainActivity.this,InstructionsActivity.class);
+                startActivity(i);
+            }
+        });
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Locale locale = new Locale("ta");
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                Intent i = new Intent(MainActivity.this,InstructionsActivity.class);
+                startActivity(i);
+            }
+        });
+        cardView3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Locale locale = new Locale("hi");
+                Locale.setDefault(locale);
+                Configuration config = new Configuration();
+                config.locale = locale;
+                getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+                Intent i = new Intent(MainActivity.this,InstructionsActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
